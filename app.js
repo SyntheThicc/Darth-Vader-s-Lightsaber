f05c5dcb4a10b02f9a9b2eae13b136398d1553ff
Vladimir Ćinić <70448614+SyntheThicc@users.noreply.github.com>
2021-06-01 17:11:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 const lightsaber = document.querySelector('.lightsaber');
 const igniteBtn = document.querySelector('.btn__ignite');
 const lightsaberSound = document.querySelector('audio');
+const colorBtns = document.querySelector('.color__btns');
+let bladeColor;
 
-
-
-
+colorBtns.addEventListener('click', function (e) {
+  bladeColor = e.target.dataset.color;
+  lightsaber.style.setProperty('--blade-color', bladeColor);
+  igniteBtn.style.setProperty('--blade-color', bladeColor);
+  console.log(bladeColor);
+});
 
 function igniteBlade() {
+  if (!bladeColor) {
+  } else {
     lightsaber.classList.add('saber__ignite');
     lightsaberSound.play();
     let playTime = (lightsaberSound.duration - 1) * 1000;
     console.log(playTime);
     setTimeout(() => {
-        lightsaber.classList.remove('saber__ignite');
+      lightsaber.classList.remove('saber__ignite');
     }, playTime);
+  }
 }
-
-
 
 igniteBtn.addEventListener('click', igniteBlade);
